f1e39bd479d7ce0b83b12a8788d73daba8a1b1f4
Himani Kashyap <saidhi009@gmail.com>
2022-02-07 23:38:52 +0530

    Beginners_MIT Classwork_Lession1&2

---
+++ Beginner_himani.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+x=1
+print(x)
+x_str=str(x)
+print("my fav num is",x,".",x,"x=1")
+print("mt fav num is", x_str, ".",x)
+
+
+# In[5]:
+
+
+text= input("type anything..")
+print(5*text)
+num=int(input("type anything.."))
+print(5*num)
+
+
+# In[6]:
+
+
+text= input("type anything..")
+print(5*text)
+num=int(input("type anything.."))
+print(5*num)
+
+
+# In[7]:
+
+
+abc<bca
+
+
+# In[8]:
+
+
+a<b
+
+
+# In[9]:
+
+
+"a"<"b"
+
+
+# In[10]:
+
+
+n=input("you are lost in forest\n*****************go left or right")
+while n== "right" or n=="Right":
+    n=input("you are lost in forest\n*****************go left or right")
+    print( "\ngo out of the lost forest!\n \O/")
+
+
+# In[12]:
+
+
+n=input("you are lost in forest\n*************************\n")
+while n== "right" or n=="Right":
+    n=input("you are lost in forest\n**********************")
+    n=input("are you mad change your plan |_|")
+    print( "\n go out of the lost forest!\n \O/")
+
+
+# In[25]:
+
+
+
+x=input(" type value....")
+while
+x==1
+print(x)
+
+
+# In[34]:
+
+
+x= int (input("x"))
+y= int (input("y"))
+print(x**y)
+print (log (x))
+
+
+# In[ ]:
+
+
+
+
